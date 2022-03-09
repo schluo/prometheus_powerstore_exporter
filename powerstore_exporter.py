bb73d7c7d0aad411598a60a4222aa7179266b1f0
Oliver Schlueter <oliver.schlueter@dell.com>
2022-03-09 09:23:19 +0100

    Update powerstore_exporter.py

--- powerstore_exporter.py
+++ powerstore_exporter.py
@@ -1,75 +1,69 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-
-import os
-import time
 import urllib3
-from powerstore import PowerStore
+import argparse
 
 from prometheus_client import start_http_server, Gauge
+from exporter import Exporter
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-DEBUG = True
+DEBUG = False
+
+def get_argument():
+
+    try:
+
+        # Setup argument parser
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-H', '--hostname',
+                            type=str,
+                            help='hostname or IP address and Port of PowerStore API',
+                            required=True)
+        parser.add_argument('-u', '--username',
+                            type=str,
+                            help='username',
+                            required=True)
+        parser.add_argument('-p', '--password',
+                            type=str,
+                            help='user password',
+                            required=True)
+        parser.add_argument('-o', '--port',
+                            type=int,
+                            help='exporter TCP Port',
+                            required=True)
+        parser.add_argument('-v', '--verbose',
+                            action='store_const', const=True,
+                            help='verbose logging',
+                            required=False)
+        parser.add_argument('-i', '--interval',
+                            type=int,
+                            help='polling interval',
+                            required=True)
+
+        args = parser.parse_args()
+
+    except KeyboardInterrupt:
+        # handle keyboard interrupt #
+        return 0
+
+    global hostname, user, password, port, interval, DEBUG
+    hostname = args.hostname
+    user = args.username
+    password = args.password
+    port = args.port
+    interval = args.interval
+    DEBUG = args.verbose
 
 
-class Exporter:
-    """
-    Representation of Prometheus metrics and loop to fetch and transform
-    powerstore metrics into Prometheus metrics.
-    """
-
-    def __init__(self, polling_interval_seconds=5):
-
-        self.polling_interval_seconds = polling_interval_seconds
-        # Prometheus metrics to collect
-
-        powerstore_hostname = str(os.getenv("POWERSTORE_HOSTNAME", "172.21.16.150"))
-        powerstore_user = str(os.getenv("POWERSTORE_USER", "admin"))
-        powerstore_password = str(os.getenv("POWERSTORE_PASSWORD", "Noah2407!"))
-        powerstore_perfstats_type = str(os.getenv("POWERSTORE_PERF_TYPE", "appliance"))
-
-        self.powerstore = PowerStore(hostname=powerstore_hostname,
-                                     user=powerstore_user,
-                                     password=powerstore_password,
-                                     perfstats_type=powerstore_perfstats_type)
-        self.powerstore.send_request_stats()
-        self.powerstore.process_stats()
-
-        self.values = []
-        for perf_key, perf_value in self.powerstore.last_stats.items():
-            self.values.append(Gauge(perf_key, perf_key))
-
-    def get_metrics_index(self, metric_name):
-        i = 0
-        for metric in self.values:
-            if metric._name == metric_name:
-                return i
-            i += 1
-
-    def run_metrics_loop(self):
-        """Metrics fetching loop"""
-
-        while True:
-            self.powerstore.send_request_stats()
-            self.powerstore.process_stats()
-            for perf_key, perf_value in self.powerstore.last_stats.items():
-                try:
-                    print(perf_key, perf_value)
-                    self.values[self.get_metrics_index(perf_key)].set(perf_value)
-                except:
-                    self.values[self.get_metrics_index(perf_key)].set(-1)
-            time.sleep(self.polling_interval_seconds)
 
 def main():
     """Main entry point"""
 
-    polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "5"))
-    exporter_port = int(os.getenv("EXPORTER_PORT", "9877"))
-    exporter = Exporter(polling_interval_seconds=polling_interval_seconds)
-
-    start_http_server(exporter_port)
+    get_argument()
+    exporter = Exporter(interval, hostname, user, password)
+    start_http_server(port)
     exporter.run_metrics_loop()
 
 if __name__ == "__main__":
